e0abca7d9e1e8b2365b122c63c2a44bc404b8b6d
VIEIRA Rafael <Rafael.VIEIRA@efsa.europa.eu>
2024-04-05 10:49:06 +0000

    adding more data

--- sql.py
+++ sql.py
@@ -3,25 +3,32 @@ import pandas as pd
 import warnings
 warnings.filterwarnings('ignore')
 
+
+'''
+1. Reading OpenFoodTox Excel File
+'''
+# reading each excel file
+subst_charact = pd.read_csv('SubstanceCharacterisation_KJ_2022.csv')
+genotoxicity = pd.read_csv('Genotoxicity_KJ_2022.csv')
+efsa_outputs = pd.read_csv('EFSAOutputs_KJ_2022.csv')
+
+#genotoxicity = pd.read_excel('Genotoxicity_KJ_2022.xlsx')
+
+'''
+2. OpenFoodTox database creation
+'''
+
 # Connecting to sqlite
-connection = sqlite3.connect('OpenFoodTox.db')
+connection = sqlite3.connect('OpenFoodTox_TEST.db')
+
 
 # Create a cursor object to insert record, cretate a table, retrieve
 cursor = connection.cursor()
 
-# Reading the data from the CSV file
-df = pd.read_csv('SubstanceCharacterisation_KJ_2022.csv')
-
-# Load df to sqlite
-df.to_sql('Substance_Characterization', connection, if_exists='replace')
-
-# Performing some queries to test the database
-data = cursor.execute('''SELECT CASNumber 
-                      FROM Substance_Characterization 
-                      WHERE Substance = 'trans-3-Hexenyl hexanoate'
-''')
-for row in data:
-    print(row)
+# Loading each dataframe to sqlite
+subst_charact.to_sql('Substance_Characterization', connection, if_exists='replace')
+genotoxicity.to_sql('Genotoxicity', connection, if_exists='replace')
+efsa_outputs.to_sql('EFSAOUTPUTS', connection, if_exists='replace')
 
 # Closing Connection
 connection.commit()
