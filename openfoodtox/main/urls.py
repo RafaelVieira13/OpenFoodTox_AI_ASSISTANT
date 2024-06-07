457b0679cbba5f97304f84a61d8fb362b9b8029b
VIEIRA Rafael <Rafael.VIEIRA@efsa.europa.eu>
2024-06-07 13:50:22 +0000

    improvements on html and css

--- openfoodtox/main/urls.py
+++ openfoodtox/main/urls.py
@@ -2,7 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("home/", views.home, name="home"),
-    path("home/v2/", views.v2, name="v2"),
+    path("", views.index, name="index")
 ]
--- openfoodtox/main/views.py
+++ openfoodtox/main/views.py
@@ -5,9 +5,3 @@ from django.http import HttpResponse
 
 def index(response):
     return render(response, "main/base.html", {})
-
-def home(response):
-    return render(response, "main/home.html", {})
-
-def v2(response):
-    return HttpResponse('<h1>v2</h1>')
