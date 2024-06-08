b0bd678dc28c85ad2007b87d64816848bc70f715
VIEIRA Rafael <Rafael.VIEIRA@efsa.europa.eu>
2024-06-08 09:23:49 +0000

    fishing html and css

--- openfoodtox/main/urls.py
+++ openfoodtox/main/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("specific", views.specific, name="specific"),
+    path("getResponse", views.getResponse, name="getResponse")
 ]
--- openfoodtox/main/views.py
+++ openfoodtox/main/views.py
@@ -4,4 +4,12 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(response):
-    return render(response, "main/base.html", {})
+    return render(response, "main/index.html", {})
+
+def specific(response):
+    return HttpResponse("list1")
+
+def getResponse(response):
+    userMessage = response.GET.get('userMessage')
+    return HttpResponse(userMessage)
+
